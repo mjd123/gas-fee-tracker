75d32f8122fd18cab9827525eb751f095b139101
Marcus <admin@Marcuss-MacBook-Pro.local>
2021-12-09 02:35:28 +0000

    set up grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Row, Col, Layout, Card } from "antd";
+
+const { Content } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Content style={{ width: "100%", padding: "10px 5px" }}>
+          <Row
+            gutter={[12, 8]}
+            style={{ width: "100%", height: "100%", margin: "0 0" }}
+            justify="space-around"
+            align="middle"
+          >
+            <Col span={12}>
+              <Card>
+                <p>Card content</p>
+                <p>Card content</p>
+              </Card>
+            </Col>
+            <Col span={12}>
+              <Card>
+                <p>Card content</p>
+                <p>Card content</p>
+              </Card>
+            </Col>
+
+            <Col span={12}>
+              <Card>
+                <p>Card content</p>
+                <p>Card content</p>
+              </Card>
+            </Col>
+            <Col span={12}>
+              <Card>
+                <p>Card content</p>
+                <p>Card content</p>
+              </Card>
+            </Col>
+          </Row>
+        </Content>
+      </Layout>
     </div>
   );
 }
